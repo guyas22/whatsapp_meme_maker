0ecbe4956014902edb77faea453b4397b1e075fb
guy.asulin <guy.asulin@ridewithvia.com>
2024-12-30 15:03:31 +0200

    fe use lambda

--- meme-ai-app/src/App.tsx
+++ meme-ai-app/src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
-const API_BASE_URL = 'http://127.0.0.1:5000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 function App() {
   // State management
